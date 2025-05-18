84cec24b8c4da0b7707ed30e271be805c642fe24
amanmdest <amanmdest@gmail.com>
2025-05-18 17:31:45 -0300

    relacionamento de entidades - with_entities, join e relationship

--- run.py
+++ run.py
@@ -1,14 +1,25 @@
 from infra.repository.filmes_repository import FilmesRepository
+from infra.repository.atores_repository import AtoresRepository
 
 repo = FilmesRepository()
-# print(repo)
+repo2 = AtoresRepository()
+print(repo) # <infra.repository.filmes_repository.FilmesRepository object at 0x7faa04db9b80>
 
-repo.insert('Duna', 'Ficcao Cientifica', 2022)
+# repo.insert('Duna', 'Ficcao Cientifica', 2022)
+# repo.insert('The Little Hours', 'Comedia', 2017)
+# repo.insert('LOTR: Two Towers', 'Fantasia', 2002) #TODO: como colocar vários gêneros
 
-repo.update('Mr. Nobody', 'Ficcao Cientifica')
+# repo.update('Mr. Nobody', 'Ficcao Cientifica')
+# repo.update('LOTR: Two Towers', 'Aventura')
 
-repo.delete('The Little Hours')
+# repo.delete('The Little Hours')
 
-data = repo.select()
+response = repo.select()
+filme = response[0]
+# print(response)
+print(filme)
+print(filme.titulo)
+print(filme.atores)
 
-print(data)
+response2 = repo2.select()
+print(response2)
