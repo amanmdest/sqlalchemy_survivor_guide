7691d85239575b073c7f929655696c9d2fdec310
amanmdest <amanmdest@gmail.com>
2025-06-03 14:11:21 -0300

    tratamento de erros; select filmes de dramas no repository

--- infra/repository/filmes_repository.py
+++ infra/repository/filmes_repository.py
@@ -1,19 +1,34 @@
 from infra.configs.connection import DBConnectionHandler
 from infra.entities.filmes import Filmes
-
+from sqlalchemy.orm.exc import NoResultFound
 class FilmesRepository:
     def select(self):
         with DBConnectionHandler() as db:
             data = db.session.query(Filmes).all()
             return data
         
+    def select_drama_movie(self):
+        with DBConnectionHandler() as db:
+            try:
+                data = db.session.query(Filmes).filter(Filmes.genero == 'Drama').one()
+                return data
+            except NoResultFound:
+                return None
+            except Exception as exception:
+                db.session.rollback()
+                raise exception
+        
     def insert(self, titulo, genero, ano):
         with DBConnectionHandler() as db:
-            data_insert = Filmes(titulo=titulo, genero=genero, ano=ano)
-            db.session.add(data_insert)
-            db.session.commit()
+            try:
+                data_insert = Filmes(titulo=titulo, genero=genero, ano=ano)
+                db.session.add(data_insert)
+                db.session.commit()
+            except Exception as exception:
+                db.session.rollback()
+                raise exception
             
-    def update(self, titulo, genero):
+    def update_gender(self, titulo, genero):
         with DBConnectionHandler() as db:
             # db.session.query(Filmes).filter(Filmes.titulo == titulo).update({"ano": ano})
             db.session.query(Filmes).filter(Filmes.titulo == titulo).update({"genero": genero})
--- run.py
+++ run.py
@@ -1,25 +1,28 @@
 from infra.repository.filmes_repository import FilmesRepository
 from infra.repository.atores_repository import AtoresRepository
 
-repo = FilmesRepository()
-repo2 = AtoresRepository()
-print(repo) # <infra.repository.filmes_repository.FilmesRepository object at 0x7faa04db9b80>
+repo_filmes = FilmesRepository()
+repo_atores = AtoresRepository()
+print(repo_filmes) # <infra.repository.filmes_repository.FilmesRepository object at 0x7faa04db9b80>
 
-# repo.insert('Duna', 'Ficcao Cientifica', 2022)
-# repo.insert('The Little Hours', 'Comedia', 2017)
-# repo.insert('LOTR: Two Towers', 'Fantasia', 2002) #TODO: como colocar vários gêneros
+# repo_filmes.insert('Duna', 'Ficcao Cientifica', 2022)
+# repo_filmes.insert('The Little Hours', 'Comedia', 2017)
+# repo_filmes.insert('LOTR: Two Towers', 'Fantasia', 2002) #TODO: como colocar vários gêneros
 
-# repo.update('Mr. Nobody', 'Ficcao Cientifica')
-# repo.update('LOTR: Two Towers', 'Aventura')
+# repo_filmes.update_gender('Mr. Nobody', 'Ficcao Cientifica')
+# repo_filmes.update_gender('LOTR: Two Towers', 'Aventura')
 
-# repo.delete('The Little Hours')
+# repo_filmes.delete('The Little Hours')
 
-response = repo.select()
+response = repo_filmes.select()
 filme = response[0]
 # print(response)
 print(filme)
 print(filme.titulo)
 print(filme.atores)
 
-response2 = repo2.select()
+response2 = repo_atores.select()
 print(response2)
+
+response3 = repo_filmes.select_drama_movie()
+print(response3)
